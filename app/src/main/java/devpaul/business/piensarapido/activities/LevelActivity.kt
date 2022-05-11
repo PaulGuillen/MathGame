da61cb1d68c870899f73570b781bf1012e95b4ec
paulguillen190 <paulguillen190@gmail.com>
2022-05-11 13:05:27 -0500

    Logic views

--- app/src/main/java/devpaul/business/piensarapido/activities/LevelActivity.kt
+++ app/src/main/java/devpaul/business/piensarapido/activities/LevelActivity.kt
@@ -1,12 +1,18 @@
 package devpaul.business.piensarapido.activities
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import devpaul.business.piensarapido.R
 
 class LevelActivity : AppCompatActivity() {
+
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level)
+        //desactivar rotacion pantalla
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
--- app/src/main/java/devpaul/business/piensarapido/activities/SettingsActivity.kt
+++ app/src/main/java/devpaul/business/piensarapido/activities/SettingsActivity.kt
@@ -1,12 +1,19 @@
 package devpaul.business.piensarapido.activities
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import devpaul.business.piensarapido.R
 
 class SettingsActivity : AppCompatActivity() {
+
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        //desactivar rotacion pantalla
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
