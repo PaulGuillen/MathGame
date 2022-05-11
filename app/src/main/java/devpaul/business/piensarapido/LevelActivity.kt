276cd9a538ddeee2902aada65f2bc08cd2a84d7c
paulguillen190 <paulguillen190@gmail.com>
2022-05-11 10:38:30 -0500

    Levels

---
+++ app/src/main/java/devpaul/business/piensarapido/LevelActivity.kt
@@ -0,0 +1,11 @@
+package devpaul.business.piensarapido
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LevelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level)
+    }
+}
