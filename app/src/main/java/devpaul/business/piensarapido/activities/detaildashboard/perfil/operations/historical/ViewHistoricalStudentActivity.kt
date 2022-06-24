211dc39731becf186f641d874d27066fcd979417
PAUL <paulguillen190@gmail.com>
2022-06-24 16:11:57 -0500

    Creating historial data per student

---
+++ app/src/main/java/devpaul/business/piensarapido/activities/detaildashboard/perfil/operations/historical/ViewHistoricalStudentActivity.kt
@@ -0,0 +1,12 @@
+package devpaul.business.piensarapido.activities.detaildashboard.perfil.operations.historical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import devpaul.business.piensarapido.R
+
+class ViewHistoricalStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_historical_student)
+    }
+}
